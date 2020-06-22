81fb2a29b5a7000c3e943573330058ce655d7fec
Roberto Barros <roberto.f.barros@gmail.com>
2020-06-22 00:38:01 -0300

    mouse grade highlight

--- main.rb
+++ main.rb
@@ -16,37 +16,9 @@ def draw_map(pos_x, pos_y, map, zoom, face)
 
   map.each do |row|
     row.each do |col|
-
       t = Tile.new(col, x, y, pos_x, pos_y, zoom: zoom, face: face)
       t.draw
-
-      # puts "x=#{x} y=#{y}"
-      # file = case col
-      #        when 1 then 'assets/landscape/landscapeTiles_075.png'
-      #        when 2 then 'assets/landscape/landscapeTiles_037.png'
-      #        when 3 then 'assets/landscape/landscapeTiles_044.png'
-      #        when 4 then 'assets/landscape/landscapeTiles_045.png'
-      #        when 5 then 'assets/landscape/landscapeTiles_052.png'
-      #        end
-
-      # x1 = pos_x + ((x * 64)-(y*64))
-      # y1 = pos_y + ((y * 32)+(x * 32))
-
-      # x2 = (pos_x + 1) + ((x * 64)-(y*64))
-      # y2 = (pos_y + 1) + ((y * 32)+(x * 32))
-
-      # x3 = (pos_x + 1) + ((x * 64)-(y*64))
-      # y3 = (pos_y + 1) + ((y * 32)+(x * 32))
-
-
-      # Image.new(file, x: x1, y: y1)
-
-      #   Line.new(
-      #     x1: x1 + 64, y1: y1,
-      #     x2: x2, y2: y2 + 32,
-      #     color: 'white',
-      #     width: 0.5
-      #   )
+      t.draw_grade if x == @local_x.to_i && y == @local_y.to_i
 
       x += 1
     end
@@ -75,6 +47,21 @@ on :key_held do |event|
 end
 
 
+on :mouse_move do |event|
+
+  global_x = event.x
+  global_y = event.y
+
+  iso_x = @start_x + 32
+  iso_y = @start_y
+
+  iso_w = (128/2) * @zoom
+  iso_h =  (64/2) * @zoom
+
+  @local_x = ((global_y - iso_y) / iso_h + (global_x - iso_x) / iso_w) / 2;
+  @local_y = ((global_y - iso_y) / iso_h - (global_x - iso_x) / iso_w) / 2;
+end
+
 on :key_down do |event|
   if event.key == 'q'
     3.times do
@@ -95,6 +82,7 @@ tick = 0
 update do
   clear
   draw_map(@start_x, @start_y, @map, @zoom, @faces.first)
+  Text.new("MOUSE: [#{@local_x.to_i},#{@local_y.to_i}]")
   tick += 1
 end
 
--- tile.rb
+++ tile.rb
@@ -20,7 +20,37 @@ class Tile
 
 
   def draw
-    i = Image.new(@file, x: @x1, y: @y1, width: IMAGE_WIDTH * @zoom, height: IMAGE_HEIGHT * @zoom)
+    Image.new( @file,
+               x: @x1,
+               y: @y1,
+               width: IMAGE_WIDTH * @zoom,
+               height: IMAGE_HEIGHT * @zoom)
+  end
+
+  def draw_grade
+    x1 = @x1 + (WIDTH/2) * @zoom
+    y1 = @y1
+
+    x2 = x1 + (WIDTH/2) * @zoom
+    y2 = y1 + (HEIGHT/2) * @zoom
+
+    x3 = x2 - (WIDTH/2) * @zoom
+    y3 = y2 + (HEIGHT/2) * @zoom
+
+    x4 = x3 - (WIDTH/2) * @zoom
+    y4 = y3 - (HEIGHT/2) * @zoom
+
+    Quad.new(
+      x1: x1, y1: y1,
+      x2: x2, y2: y2,
+      x3: x3, y3: y3,
+      x4: x4, y4: y4,
+      color: 'red',
+      opacity: 0.5
+    )
+
+
+
   end
 
 end
