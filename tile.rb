5210526e43809b335751f06f0f0da9dc2b66d5fe
Roberto Barros <roberto.f.barros@gmail.com>
2020-06-21 22:27:01 -0300

    simplify coordenate system

--- tile.rb
+++ tile.rb
@@ -8,17 +8,14 @@ class Tile
 
 
   def initialize(id, row, col, start_x, start_y, zoom: 1, face: :north)
-
     @zoom = zoom
     @file = case id
             when 1 then Asset.get_by(:grass, face)
             when 2 then Asset.get_by(:road, face)
             end
 
-    @x1 = start_x + ((row * (WIDTH/2) * @zoom) - ( col * (WIDTH/2) * @zoom))
-    @y1 = start_y + ((row * (HEIGHT/2) * @zoom) +(col * (HEIGHT/2) * @zoom))
-
-
+    @x1 = start_x + ((row - col) * WIDTH/2 * @zoom)
+    @y1 = start_y + ((row + col) * HEIGHT/2 * @zoom)
   end
 
 
